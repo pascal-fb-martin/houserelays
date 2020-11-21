29767a4a8beb2e4a9a47db1ed6e3e838523be779
Pascal Martin <pascal.fb.martin@gmail.com>
2020-11-20 17:04:54 -0800

    Allow cross domain GET requests

--- houserelays.c
+++ houserelays.c
@@ -181,6 +181,23 @@ static void hc_background (int fd, int mode) {
     houselog_background (now);
 }
 
+static void relays_protect (const char *method, const char *uri) {
+
+    const char *origin = echttp_attribute_get ("Origin");
+    if (!origin) return; // Not a cross-domain request.
+
+    if (!strcmp (method, "GET")) {
+        echttp_attribute_set ("Access-Control-Allow-Origin", "*");
+        return;
+    }
+    if (!strcmp (method, "OPTIONS")) {
+        echttp_attribute_set ("Access-Control-Allow-Origin", "*");
+        echttp_error (204, "No Content"); // Not an error, but don't process.
+        return;
+    }
+    echttp_error (403, "Forbidden Cross-Domain");
+}
+
 int main (int argc, const char **argv) {
 
     // These strange statements are to make sure that fds 0 to 2 are
@@ -210,6 +227,8 @@ int main (int argc, const char **argv) {
         houselog_trace
             (HOUSE_FAILURE, "CONFIG", "Cannot configure GPIO: %s\n", error);
     }
+
+    echttp_protect (0, relays_protect);
 
     echttp_route_uri ("/relays/status", relays_status);
     echttp_route_uri ("/relays/set",    relays_set);
