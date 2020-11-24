f3fa1d455ec89338560ba75b5d095e13f04fd1dd
Pascal Martin <pascal.fb.martin@gmail.com>
2020-11-24 13:06:15 -0800

    Add link back to Portal

--- houserelays.c
+++ houserelays.c
@@ -72,9 +72,10 @@ static const char *relays_status (const char *method, const char *uri,
     ParserContext context = echttp_json_start (token, 1024, pool, 65537);
 
     int root = echttp_json_add_object (context, 0, 0);
+    echttp_json_add_string (context, root, "host", HostName);
+    echttp_json_add_string (context, root, "proxy", houseportal_server());
+    echttp_json_add_integer (context, root, "timestamp", (long)time(0));
     int top = echttp_json_add_object (context, root, "control");
-    echttp_json_add_integer (context, top, "timestamp", (long)time(0));
-    echttp_json_add_string (context, top, "host", HostName);
     int container = echttp_json_add_object (context, top, "status");
 
     for (i = 0; i < count; ++i) {
